a66ec06a3506e42477d56eeefa41694d1fcb47a0
Pablo <contactoemail.pablo@gmail.com>
2022-07-09 15:08:50 +0200

    Solving Hydration problem Navbar 1.1

--- components/molecules/UserIcon.tsx
+++ components/molecules/UserIcon.tsx
@@ -1,18 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
-interface UserIconProps {
-  w: number;
-  h: number;
-}
-const UserIcon = ({ w, h }: UserIconProps) => {
-  const url =
-    "https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg";
+const UserIcon = () => {
   return (
-    <div className="p-4 cursor-pointer hover:saturate-150 opacity-95 hover:opacity-100 hover:scale-105 transition-all duration-150 ease-in-out">
-      <Link href="/login">
-        <img src={url} alt="user-icon" width={w} height={h} />
-      </Link>
-    </div>
+    <>
+      <h1>Hello</h1>
+    </>
   );
 };
 export default UserIcon;
